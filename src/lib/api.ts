5a55266b205677ad5eeb09f2f4a73740ff5f133d
marialuiza2a <mluizasenai2@gmail.com>
2024-04-23 15:44:32 -0300

    finalizando

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.API_URL || 'http://localhost/zerograu-backend/';
+const API_URL = process.env.API_URL || 'https://acreanofit.com/zerograu-backend/';
 
 const api = axios.create({
   baseURL: API_URL
